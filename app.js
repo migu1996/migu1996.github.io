1467b211809ce38d6bf818821a6cb42f66be14a3
migu1996 <96053526+migu1996@users.noreply.github.com>
2021-12-28 17:45:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -20,3 +20,17 @@ const navSlide = () => {
     });
 }
 navSlide();
+
+var indexValue = 0;
+function slideShow(){
+    setTimeout(slideShow, 4000);
+    var x;
+    const img = document.querySelectorAll("img");
+    for(x = 0; x < img.length; x++){
+        img[x].style.display = "none";
+    }
+    indexValue ++;
+    if(indexValue > img.length) {indexValue = 1}
+    img[indexValue - 1].style.display = "block";
+}
+slideShow();
